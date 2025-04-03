0e9720052419ce21b90be0f13273dc8140885624
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-04-03 20:49:10 -0300

    exercicio 2 finalizado

---
+++ capitulo-01/exercicio2.py
@@ -0,0 +1,7 @@
+#Faça um programa que peça para o usuário digitar três valores inteiro e imprima a soma deles.
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+numero3 = int(input("Digite o terceiro número:"))
+
+print("A soma dos números digitados é:", numero1+numero2+numero3)
