9c448baafc3b694e7359758c521a0a89bd71b18c
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-04-03 21:05:30 -0300

    finalizado exercicio 3

---
+++ capitulo-01/exercicio3.py
@@ -0,0 +1,8 @@
+#Faça um programa que recebe três valores e apresente a soma dos quadrados dos valores lidos
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+numero3 = int(input("Digite o terceiro número: "))
+
+print("Os número digitados foram:", numero1, numero2, numero3)
+print("A soma do quadrado dos três número é:", (numero1**2)+(numero2**2)+(numero3**2))
