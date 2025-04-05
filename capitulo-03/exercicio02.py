44b9228592c977c0d8f391ca25ed5b552a5f69ee
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-04-05 04:58:32 -0300

    Finalizado exercicio02-c03

--- capitulo-03/exercicio02.py
+++ capitulo-03/exercicio02.py
@@ -1,2 +1,10 @@
 #Faça um programa que utilize o comando while para mostrar na tela uma contagem regressiva, iniciando
 #em 10 e terminando em 0. Mostre também uma mensagem “FIM!” após a contagem.
+
+contador = 0
+
+while contador <= 10:
+    print((10-contador))
+    contador += 1
+
+print("FIM!")
