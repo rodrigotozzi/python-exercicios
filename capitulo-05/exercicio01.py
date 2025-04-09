cd74aacb4997364e2860497b6d9bab292c792617
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-04-09 14:56:53 -0300

    Finalizado exercicio01-c05

---
+++ capitulo-05/exercicio01.py
@@ -0,0 +1,9 @@
+#Crie um programa que tenha uma função que recebe um parâmetro inteiro e devolve o seu dobro.
+
+def dobro(valor):
+    resultado = int(valor) * 2
+    print(resultado)
+
+numero = input("Digite o valor que quer descobrir o seu dobro: ")
+
+dobro(numero)
