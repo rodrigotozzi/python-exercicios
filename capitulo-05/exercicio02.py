910ac9e3dc6f6b0ad4fc0e89ead78e5a3b1aa482
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-04-09 15:25:52 -0300

    Iniciado exercicio02-c05

---
+++ capitulo-05/exercicio02.py
@@ -0,0 +1,33 @@
+# Faça um programa que tenha uma função que recebe uma data no formato string exemplo “01/01/2024” e
+# imprima ela por extenso como “1 de janeiro de 20204”.
+
+def data_extenso():
+    dia = input("Informe o dia: ")
+    mes = input("Informe o mês: ")
+    if mes == "01":
+        mes = "Janeiro"
+    elif mes == "02":
+        mes = "Fevereiro"
+    elif mes == "03":
+        mes = "Março"
+    elif mes == "04"
+        mes = "Abril"
+    elif mes == "05":
+        mes = "Maio"
+    elif mes == "06":
+        mes = "Junho"
+    elif mes == "07":
+        mes = "Julho"
+    elif mes == "08":
+        mes = "Agosto"
+    elif mes == "09":
+        mes = "Setembro"
+    elif mes == "10":
+        mes = "Outubro"
+    elif mes == "11":
+        mes = "Novembro"
+    elif mes == "12":
+        mes = "Dezembro"
+    ano = input("Informe o ano: ")
+
+print(f"")
