e9b24cedc8f59f58a3cf03ab39bfd734dfc317eb
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-04-10 21:17:52 -0300

    Finalizado exercicio03-c05

--- capitulo-05/exercicio03.py
+++ capitulo-05/exercicio03.py
@@ -1,1 +1,18 @@
 #Faça um programa que tenha uma função que receba uma lista de inteiros e retorne o maior valor.
+
+def lista_maior_valor():
+    maior_valor = 0
+    lista = []
+    while True:
+        valor = input("Informe um valor para a lista ou digite SAIR: ")
+        if valor.upper() == "SAIR":
+            break
+        else:
+            lista.append(int(valor))
+
+    for i in lista:
+        if i > maior_valor:
+            maior_valor = i
+    return maior_valor
+
+print(lista_maior_valor())
