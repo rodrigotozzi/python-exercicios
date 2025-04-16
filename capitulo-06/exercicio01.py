79360b2ff3eecb957311845e9b2e0b5d86346ca8
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-04-16 06:05:21 -0300

    Finalizado exercicio01-c06

---
+++ capitulo-06/exercicio01.py
@@ -0,0 +1,29 @@
+# Crie uma classe Pessoa, contendo nome, data de nascimento e email. Crie as propriedades getters e
+# setters para os atributos e um metodo para imprimir os dados de uma pessoa.
+
+class Pessoa:
+
+    def __init__(self, nome, nascimento, email):
+        self.nome = nome
+        self.nascimento = nascimento
+        self.email = email
+
+    def definir_nome(self):
+        self.nome = input("Informe o nome: ")
+
+    def definir_email(self):
+        self.email = input("Infome o e-mail: ")
+
+    def definir_nascimento(self):
+        self.nascimento = input("Informe a data de nascimento: ")
+
+    def mostrar_dados(self):
+        print(f'{self.nome} nasceu em {self.nascimento} e tem o e-mail {self.email}')
+
+pessoa1 = Pessoa("Rodrigo", "22/12/1990", "rodrigo@hotmail.com")
+pessoa1.mostrar_dados()
+
+pessoa1.definir_nome()
+pessoa1.definir_nascimento()
+pessoa1.definir_email()
+pessoa1.mostrar_dados()
