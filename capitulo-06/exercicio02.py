2078bbd5d63fdec0c64996f11cfddbb2f4938034
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-04-17 05:19:54 -0300

    Refatorado exercicio02-c06

--- capitulo-06/exercicio02.py
+++ capitulo-06/exercicio02.py
@@ -12,15 +12,33 @@ class Agenda:
     def __init__(self):
         self.contato = []
 
+    #Armazenar Contato
     def adicionar_contato(self):
         self.contato.append(input("Informe o nome do contato: "))
 
+    #Remover Contato
     def remover_contato(self):
         deletar = input("Informe o contato que gostaria de deletar: ")
-        for i in self.contato:
-            if i == deletar:
-                self.contato.pop(deletar)
+        if deletar in self.contato:
+            self.contato.remove(deletar)
+        else:
+            print(f'Contato "{deletar}" não encontrado')
 
+    #Informa em que posição da agenda está o contato
+    def buscar_contato(self):
+        contador = 0
+        buscar = input("Informe o nome do contato que deseja saber o indice: ")
+        if buscar not in self.contato:
+            print("Contato não localizado")
+        else:
+            for i in self.contato:
+                contador += 1
+            for i in range(contador):
+                for x in self.contato:
+                    if x == buscar:
+                        print(f'O contato {buscar} está no indice {i}')
+
+    #Imprimir Agenda
     def imprimir_agenda(self):
         print("Esses são os contatos da agenda:")
         for i in self.contato:
@@ -32,5 +50,11 @@ agenda1.adicionar_contato()
 agenda1.adicionar_contato()
 agenda1.imprimir_agenda()
 
+#TESTE BUSCAR
+agenda1.buscar_contato()
+
+"""
+#TESTE REMOVER
 agenda1.remover_contato()
 agenda1.imprimir_agenda()
+"""
