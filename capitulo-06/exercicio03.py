fd41f4c65265f4d78d1318b6abc781f50edebd6f
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-04-22 12:19:56 -0300

    criado exercicio03-c06
    criado funções da televisao

--- capitulo-06/exercicio03.py
+++ capitulo-06/exercicio03.py
@@ -6,3 +6,76 @@ b) O controle de volume permite aumentar ou diminuir o volume de som em uma unid
 c) O controle de canal permite aumentar ou diminuir o canal em uma unidade de cada vez mas também
 permitir que seja informado um número de canal para efetuar a troca;
 """
+
+class Televisao:
+
+    def __init__(self):
+        self.status = False
+        self.volume: int = 0
+        self.canal: int = 0
+
+    def estado_atual(self):
+        print(f'A Televisão está {self.status}, no volume {self.volume} e canal {self.canal}')
+
+    def liga_desliga(self):
+        if self.status:
+            self.status = False
+        else:
+            self.status = True
+
+    def aumentar_volume(self):
+        if self.volume == 10:
+            print("Volume no nível máximo")
+        elif 0 <= self.volume < 9:
+            self.volume += 1
+            print("Aumentou 1 unidade de volume")
+        print(f'Volume Atual {self.volume}')
+
+    def diminuir_volume(self):
+        if self.volume == 0:
+            print("Volume no nível mínimo")
+        elif 0 < self.volume <= 10:
+            self.volume -= 1
+            print("Diminui 1 unidade de volume")
+        print(f'Volume Atual {self.volume}')
+
+    def aumentar_canal(self):
+        if self.canal == 100:
+            self.canal = 0
+        elif 0 <= self.canal < 100:
+            self.canal += 1
+            print("Subiu 1 unidade do canal")
+        print(f'Canal Atual {self.canal}')
+
+    def diminuir_canal(self):
+        if self.canal == 0:
+            self.canal = 100
+        elif 0 < self.canal <= 100:
+            self.canal -= 1
+            print("Diminuiu 1 unidade do canal")
+        print(f'Canal Atual {self.canal}')
+
+tv1 = Televisao()
+tv1.estado_atual()
+
+
+
+
+"""
+TESTE DIMINUIR CANAL
+tv1.diminuir_canal()
+
+TESTE AUMENTAR CANAL
+tv1.aumentar_canal()
+
+TESTE DIMINUIR VOLUME
+tv1.diminuir_volume()
+
+TESTE AUMENTAR VOLUME
+tv1.aumentar_volume()
+
+TESTE LIGA / DESLIGA
+tv1.liga_desliga()
+tv1.estado_atual()
+"""
+
