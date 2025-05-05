5cbc0008385213faf6f7e2c60e360e7837b01fce
Rodrigo Chaves Tozzi <rodrigotozzi@hotmail.com.br>
2025-05-05 15:17:53 -0300

    finalizado exercicio01-c07

--- capitulo-07/exercicio-01.py
+++ capitulo-07/exercicio-01.py
@@ -6,21 +6,37 @@ setters para os atributos e um método para imprimir os dados de uma pessoa.
 class Pessoa:
 
     def __init__(self, nome, nascimento, email):
-        self.__nome = nome
-        self.__nascimento = nascimento
-        self.__email = email
+        self._nome = nome
+        self._nascimento = nascimento
+        self._email = email
 
     @property
-    def informe_nome(self):
-        return self.__nome
+    def nome(self):
+        return self._nome
 
-    @informe_nome.setter
-    def informe_nome(self, novo_nome):
-        self.__nome = novo_nome
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+
+    @property
+    def nascimento(self):
+        return self._nascimento
+
+    @nascimento.setter
+    def nascimento(self, novo_nascimento):
+        self._nascimento = novo_nascimento
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, novo_email):
+        self._email = novo_email
+
+    @property
+    def dados_pessoal(self):
+        return f'Nome: {self._nome}\nData de Nascimento: {self._nascimento}\nEmail: {self._email}'
 
 
-pessoa1 = Pessoa("Rodrigo", "Dezembro", "r@gmail.com")
-print(pessoa1.informe_nome)
-
-pessoa1 = pessoa1.informe_nome()
-
+pessoa1 = Pessoa("Rodrigo", "Dezembro", "@gmail.com")
