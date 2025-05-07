48504d8c0b15c187c6a3a4a2b5799f3cb78bec91
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-05-07 01:20:06 -0300

    finalizado exercicio01-c07

--- capitulo-07/exercicio-01.py
+++ capitulo-07/exercicio-01.py
@@ -3,3 +3,30 @@ Crie a classe Veiculo, contendo marca e modelo. Crie as propriedades getters e s
 um método para imprimir os dados de um veículo. Crie também o construtor da classe.
 """
 
+class Veiculo:
+    def __init__(self, marca, modelo):
+        self._marca = marca
+        self._modelo = modelo
+
+    @property
+    def marca(self):
+        return self._marca
+
+    @marca.setter
+    def marca(self, nova_marca):
+        self._marca = nova_marca
+
+    @property
+    def modelo(self):
+        return self._modelo
+
+    @modelo.setter
+    def modelo(self, novo_modelo):
+        self._modelo = novo_modelo
+
+    @property
+    def dados_veiculo(self):
+        return self._marca, self._modelo
+
+veiculo1 = Veiculo("Ford", "Fiesta")
+print(veiculo1.dados_veiculo)
