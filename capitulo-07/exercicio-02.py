b28820e9deea74ed8e3c60404ac32728a067c2e5
Rodrigo Tozzi <rodrigotozzi@hotmail.com.br>
2025-05-07 17:14:47 -0300

    finalizado exercicio03-c07

--- capitulo-07/exercicio-02.py
+++ capitulo-07/exercicio-02.py
@@ -3,3 +3,37 @@ Crie a classe Carro que herda a classe Veiculo e possui o atributo portas. Crie 
 setters para o atributo. Crie também o construtor da classe. Sobrescreva o método de imprimir os dados do
 veículo de forma a apresentar também a quantidade de portas do carro.
 """
+
+class Veiculo:
+    def __init__(self, marca, modelo):
+        self._marca = marca
+        self._modelo = modelo
+
+    @property
+    def marca(self):
+        return self._marca
+
+    @marca.setter
+    def marca(self, nova_marca):
+        self._marca = nova_marca
+
+    @property
+    def modelo(self):
+        return self._modelo
+
+    @modelo.setter
+    def modelo(self, novo_modelo):
+        self._modelo = novo_modelo
+
+    @property
+    def dados_veiculo(self):
+        return self._marca, self._modelo
+
+class Carro(Veiculo):
+    def __init__(self, marca, modelo, porta):
+        super().__init__(marca, modelo)
+        self._porta = porta
+
+    @property
+    def dados_veiculo(self):
+        return f'Marca: {self._marca} Modelo: {self._modelo} Quantidade de Portas: {self._porta}'
--- capitulo-07/exercicio-03.py
+++ capitulo-07/exercicio-03.py
@@ -1,3 +1,45 @@
 """
 Crie um programa, instancie um objeto da classe Carro e teste o seu médodo.
 """
+
+class Veiculo:
+    def __init__(self, marca, modelo):
+        self._marca = marca
+        self._modelo = modelo
+
+    @property
+    def marca(self):
+        return self._marca
+
+    @marca.setter
+    def marca(self, nova_marca):
+        self._marca = nova_marca
+
+    @property
+    def modelo(self):
+        return self._modelo
+
+    @modelo.setter
+    def modelo(self, novo_modelo):
+        self._modelo = novo_modelo
+
+    @property
+    def dados_veiculo(self):
+        return self._marca, self._modelo
+
+class Carro(Veiculo):
+    def __init__(self, marca, modelo, porta):
+        super().__init__(marca, modelo)
+        self._porta = porta
+
+    @property
+    def dados_veiculo(self):
+        return f'Marca: {self._marca} Modelo: {self._modelo} Quantidade de Portas: {self._porta}'
+
+
+print("Vamos criar o seu carro...")
+marca = input("Informe a marca:")
+modelo = input("Informe o modelo: ")
+porta = input("Informe a quantidade de portas: ")
+carro = Carro(marca, modelo, porta)
+print(carro.dados_veiculo)
